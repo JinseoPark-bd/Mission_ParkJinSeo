89a0d2c914814e7f5c350326b7addb15c98ab2ba
JinseoPark-bd <wlstjdhkd@naver.com>
2023-05-10 16:51:34 +0900

    LikeablePersonController에서 showToList에서 특정성별 호감 필터링 처리

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -16,6 +16,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 @RequestMapping("/usr/likeablePerson")
@@ -122,13 +124,24 @@ public class LikeablePersonController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/toList")
-    public String showToList(Model model) {
+    public String showToList(Model model, String gender, String attractiveTypeCode, String sortCode) {
         InstaMember instaMember = rq.getMember().getInstaMember();
 
         // 인스타인증을 했는지 체크
         if (instaMember != null) {
             // 해당 인스타회원이 좋아하는 사람들 목록
-            List<LikeablePerson> likeablePeople = instaMember.getToLikeablePeople();
+            Stream<LikeablePerson> likeablePeopleStream = instaMember.getToLikeablePeople().stream();
+
+            if(gender != null) {
+                if(gender == "M") {
+                    likeablePeopleStream = likeablePeopleStream.filter(e -> e.getFromInstaMember().getGender() == "M");
+                } else {
+                    likeablePeopleStream = likeablePeopleStream.filter(e -> e.getFromInstaMember().getGender() == "W");
+                }
+            }
+
+            List<LikeablePerson> likeablePeople = likeablePeopleStream.collect(Collectors.toList());
+
             model.addAttribute("likeablePeople", likeablePeople);
         }
 
